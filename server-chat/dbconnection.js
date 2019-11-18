a4d5b2b461b1bf27318662a0f3daadeb5b63a3c5
Andres Vargas <tcarlosvargas@gmail.com>
2019-11-18 08:38:53 -0500

    .

--- server-chat/dbconnection.js
+++ server-chat/dbconnection.js
@@ -1,5 +1,5 @@
 const  mongoose  = require("mongoose");
 mongoose.Promise  = require("bluebird");
-const  url  =  "mongodb://localhost:27017/chat";
+const  url  =  "mongodb://ec2-34-238-115-63.compute-1.amazonaws.com:27017/chat";
 const  connect  =  mongoose.connect(url, { useNewUrlParser: true  });
 module.exports  =  connect;
--- webapp-front/src/environments/environment.prod.ts
+++ webapp-front/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   API_URL: 'http://ec2-34-238-115-63.compute-1.amazonaws.com:8080/backend',
   SoketIoConfig: { 
-  	url: 'http://localhost:500',
+  	url: 'http://ec2-34-238-115-63.compute-1.amazonaws.com:500',
   	options: {}
   }
 };
