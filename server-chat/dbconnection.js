45179ea656cb41853dde38cd50170f96786231c4
Andres Vargas <tcarlosvargas@gmail.com>
2019-11-19 08:11:44 -0500

    .

--- server-chat/dbconnection.js
+++ server-chat/dbconnection.js
@@ -1,5 +1,5 @@
 const  mongoose  = require("mongoose");
 mongoose.Promise  = require("bluebird");
-const  url  =  "mongodb://ec2-54-89-251-211.compute-1.amazonaws.com:27017/chat";
-const  connect  =  mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true,  });
+const  url  =  "mongodb://localhost:27017/chat";
+const  connect  =  mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true  });
 module.exports  =  connect;
