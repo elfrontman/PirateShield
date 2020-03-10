3ab73eda5ea8b36ffa1ba3aa1f497aec0a6a35a6
Andres Vargas <tcarlosvargas@gmail.com>
2020-03-09 19:27:10 -0500

    Update operativos.py

--- backend/views/operativos.py
+++ backend/views/operativos.py
@@ -84,7 +84,6 @@ def operativo_edit(request, pk):
             operativo.activation = request.POST['activation_date']
             operativo.name = request.POST['name']
             operativo.description = request.POST['description']
-            operativo.token = get_random_string(length=6)
             operativo.brandsList = ','.join(request.POST.getlist('brands[]'))
             operativo.productList = ','.join(request.POST.getlist('products[]'))
 
