57ad499bda6c23ab10bd4e581f3b1d9e4dadb717
Andres Vargas <tcarlosvargas@gmail.com>
2020-03-29 20:13:49 -0500

    -

--- static/backend/js/app.js
+++ static/backend/js/app.js
@@ -1,5 +1,17 @@
 $(document).foundation()
 
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
+
 $(document).ready(function(){
 	var input_file = $('[type=file]').parent('p')
 	input_file.addClass('input-file');
