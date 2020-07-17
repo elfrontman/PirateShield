3d0c0fd61f368f43dd5ee5c870d6fac4bf644620
jromeroc <jcamiloromeroc@gmail.com>
2020-07-17 10:40:27 -0500

    Cambio setting DB

--- pirateshield/settings.py
+++ pirateshield/settings.py
@@ -87,8 +87,11 @@ WSGI_APPLICATION = 'pirateshield.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            #'read_default_file': 'mysql.cnf',
+            'read_default_file': '/etc/mysql/myconf_demo.cnf',
+        }
     }
 }
 
