88b4898832d63255cca9ea92c5969f1b7f923f0c
Andres Vargas <tcarlosvargas@gmail.com>
2020-08-21 10:15:20 -0500

    Update urls.py

--- pirateshield/urls.py
+++ pirateshield/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('', lambda req: redirect('/backend/')),
