8b60145a3ac5a6d5e55ae7f442d4eddc9328f646
Andrés Vargas <tcarlosvargas@gmail.com>
2021-05-27 20:39:11 -0500

    Update chat.js

--- static/backend/js/chat.js
+++ static/backend/js/chat.js
@@ -1,12 +1,12 @@
 (function() {
-	//var ip_chat = 'http://190.60.205.188:500';
-	//var ip_service = 'http://190.60.205.188:8080/backend';
+	var ip_chat = 'http://190.60.205.188:500';
+	var ip_service = 'http://190.60.205.188/agatha/backend';
 	
 	// var ip_chat = 'http://192.168.0.16:500';
 	// var ip_service = 'http://192.168.0.16:8000/backend';
 
-	var ip_chat = 'http://localhost:500';
-	var ip_service = 'http://localhost:8000/backend';
+	// var ip_chat = 'http://localhost:500';
+	// var ip_service = 'http://localhost:8000/backend';
 
 
 	var  socket = io(ip_chat);
